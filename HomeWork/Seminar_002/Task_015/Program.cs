2aef4d0f5a1b4428896831e93eaf372010dcf326
RemoveFire <siv.vitya@yandex.ru>
2022-09-22 03:39:32 +0300

    Added a second homework assignment in the Seminar_002 folder , which includes 3 assignments(Task_010,_013,_015)

---
+++ HomeWork/Seminar_002/Task_015/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 15: 
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+ 6 -> да
+ 7 -> да
+ 1 -> нет
+ */
+Console.Clear();
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine($"Под цифрой {dayNumber} - Выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("это вообще не день недели");
+  }
+  else Console.WriteLine($"Под цифрой {dayNumber} - Рабочий");
+
+
+
+
+    if (dayNumber > 0 && dayNumber < 8)
+    {
+        if (dayNumber == 7 || dayNumber == 6)
+        {
+            Console.Write($"Под цифрой {dayNumber} - Выходной");
+        }
+        else
+        {
+            Console.Write($"Под цифрой {dayNumber} - Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
+    }
+    
+
