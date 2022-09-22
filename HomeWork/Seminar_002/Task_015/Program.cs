160ec9a39274582e914560b13caa7f0a326aa670
RemoveFire <siv.vitya@yandex.ru>
2022-09-22 20:21:19 +0300

    up home work

--- HomeWork/Seminar_002/Task_015/Program.cs
+++ HomeWork/Seminar_002/Task_015/Program.cs
@@ -6,35 +6,60 @@
  */
 Console.Clear();
 
-Console.Write("Введи цифру, обозначающую день недели: ");
+Console.Write("Введите число, обозначающее день недели: ");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
 
-  if (dayNumber == 6 || dayNumber == 7) {
-  Console.WriteLine($"Под цифрой {dayNumber} - Выходной");
+if (dayNumber == 6 || dayNumber == 7) 
+  {
+        Console.WriteLine($"Под числом {dayNumber} - Выходной");
   }
-  else if (dayNumber < 1 || dayNumber > 7) {
-    Console.WriteLine("это вообще не день недели");
+else if (dayNumber < 1 || dayNumber > 7) 
+  {
+        Console.WriteLine($"Число {dayNumber} выходит за значение дня недели. Повтороите попытку и введите корректные данные");
   }
-  else Console.WriteLine($"Под цифрой {dayNumber} - Рабочий");
+else Console.WriteLine($"Под числом {dayNumber} - Рабочий");
 
 
 
 
-    if (dayNumber > 0 && dayNumber < 8)
+if (dayNumber > 0 && dayNumber < 8)
+{
+    if (dayNumber == 7 || dayNumber == 6)
     {
-        if (dayNumber == 7 || dayNumber == 6)
-        {
-            Console.Write($"Под цифрой {dayNumber} - Выходной");
-        }
-        else
-        {
-            Console.Write($"Под цифрой {dayNumber} - Рабочий");
-        }
+        Console.Write($"Под числом {dayNumber} - Выходной");
     }
     else
+    {
+        Console.Write($"Под числом {dayNumber} - Рабочий");
+    }
+}
+else
     {
         Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
     }
     
 
+//Другой способ решения
+Console.WriteLine("   ");
+
+Console.WriteLine("  **Другой способ решения***  ");
+
+Console.WriteLine("   ");
+Console.Write("Введите число, обозначающее день недели: ");
+dayNumber = int.Parse(Console.ReadLine()!);
+
+if(dayNumber <= 0)
+{
+    Console.WriteLine("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
+}
+else if(dayNumber > 7)
+{
+    Console.WriteLine("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
+}
+else if(dayNumber <= 5)
+{
+    Console.WriteLine("Будний день");
+}
+else
+Console.WriteLine("Выходной");
